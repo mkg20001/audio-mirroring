8e84726475983e20a40c4a387d6a00d5ccd2d940
Maciej Krüger <mkg20001@gmail.com>
2025-07-05 16:29:48 +0200

    Initial Commit

---
+++ package.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  cargo,
+  desktop-file-utils,
+  fetchFromGitLab,
+  glib,
+  gtk4,
+  libadwaita,
+  meson,
+  ninja,
+  pipewire,
+  pkg-config,
+  rustPlatform,
+  rustc,
+  stdenv,
+  wrapGAppsHook4,
+}:
+
+stdenv.mkDerivation rec {
+  name = "audio-sharing";
+
+  src = ./.;
+
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./Cargo.lock;
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    rustPlatform.cargoSetupHook
+    cargo
+    rustc
+    rustPlatform.bindgenHook
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    desktop-file-utils
+    glib
+    gtk4
+    libadwaita
+    pipewire
+  ];
+
+  meta = with lib; {
+    description = "Audio Sharing for Linux";
+    homepage = "https://github.com/mkg20001/audio-sharing";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ mkg20001 ];
+    platforms = platforms.linux;
+    mainProgram = "audiosharing";
+  };
+}
